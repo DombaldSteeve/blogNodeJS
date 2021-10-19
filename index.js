4c8812797012ad26e68d852737ffb9be9d7201e5
Steeve <steeve.dombald.pro@outlook.com>
2021-10-19 18:36:00 +0200

    projet fini

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 import express from 'express';
 import path from 'path';
 import ejs from 'ejs';
+import crypto from 'crypto';
 
 import mongoose from 'mongoose';
+import BlogPost from './models/blogPost.js';
+import User from './models/register.js';
+
 mongoose.connect('mongodb://localhost:27017/blogdb' , {useNewUrlParser:true});
 
 
@@ -25,20 +29,43 @@ app.get('/about', (req,res) => {
 app.get('/contact', (req,res) => {
     res.render('contact')
 })
-app.get('/', (req,res) => {
-    res.render('index')
-})
-app.get('/post', (req,res) => {
-    res.render('post')
-})
+
+// app.get('/post', (req,res) => {
+//     res.render('post')
+// })
 app.get('/post/new', (req,res) => {
     res.render('create');
 })
 
-app.post('/posts/store', async (req,res) =>{
-    console.log(req.body);
-    console.log(req.body.title);
-    console.log(req.body.content);
-
+app.post('/posts/store', (req, res)=>{
+    BlogPost.create({
+        title: req.body.title,
+        body: req.body.body,
+    }, (error, blogspost)=>{console.log(error, blogspost)});
     res.redirect('/');
 })
+
+app.get('/', async (req,res) => {
+    const blogposts = await BlogPost.find({ });
+    res.render('index', {blogposts});
+})
+
+app.get('/post/:id', async (req,res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post' , {blogpost});
+})
+
+app.get('/register', (req,res) => {
+    res.render('enregistrement');
+});
+
+
+app.post('/user/login',  (req,res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    user.save(); 
+})
+
+
--- models/blogPost.js
+++ models/blogPost.js
@@ -5,8 +5,13 @@ mongoose.connect('mongodb://localhost:27017/blogdb', {useNewUrlParser:true});
 // Model puis intentiation
 const blogPostSchema = new mongoose.Schema({
     title: String,
-    body: String
-  });
+    body: String,
+    userName: String,
+    postedDate: {
+      type: Date,
+      default: new Date(),
+    }
+  });     
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
